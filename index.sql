3cadce9cc4cd347ab7f8c50710da76e45d4254b1
Aruna-Sankari <arunaponrajkdm@gmail.com>
2024-02-11 19:02:01 +0530

    completed

---
+++ index.sql
@@ -0,0 +1,16 @@
+--Multiple Row Function
+
+-->count
+select count(*) from student;
+
+-->Min
+select Min(age) from student;
+
+-->Max
+select Max(marks) from student;
+
+-->Sum
+select sum(marks) from student;
+
+-->avg
+select avg(marks) from student;
